d7f49ffb94e84b3b9e8a4e0c37fe8123f65b9995
Aditya Kumar <adityaforwork101@gmail.com>
2024-12-01 18:30:29 +0530

    Trying basics

---
+++ NewOne.java
@@ -0,0 +1,26 @@
+// import java.util.Scanner;
+
+// class NewOne {
+//     public static void main(String args[]) {
+//         // System.out.println("HEllo world");
+//         // System.out.println(7+9);
+
+//         // int a = 33;
+//         // int b = 67;
+
+//         // System.out.println("sum of a and b is : " + (a+b));
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the radius of the circle : ");
+//         float radius = sc.nextFloat();
+
+//         float area = 3.14f*radius*radius;
+//         System.out.println("radius = " + radius);
+//         System.out.println("area = " + area);
+
+
+//         sc.close();
+
+        
+//     }
+// }
