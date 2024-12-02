6e3f9192d2805353b36764bdb884a04950882a6e
Aditya Kumar <adityaforwork101@gmail.com>
2024-12-02 14:20:46 +0530

    Chapter 1(Basics)

---
+++ ChapterOne.java
@@ -0,0 +1,45 @@
+class ChapterOne {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        //question 1 : sum of three no.s
+        // System.out.print("Enter the value of three operands : ");
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // System.out.println("\n Sum = " + (a+b+c));
+
+
+        //question 2 : calculate CGPA of three subjects each out of 100 
+        // float hindi, english, maths, sum, cgpa;
+        // System.out.println("Enter the marks of hindi, english, and maths : ");
+        // hindi = sc.nextFloat();
+        // english = sc.nextFloat();
+        // maths = sc.nextFloat();
+        // sum = hindi + english + maths;
+        // float percentage = sum/3;
+        // cgpa = percentage/9.5f;
+            // System.out.println("CGPA of the student is : " + cgpa); 
+
+
+        //question 3 : take user's name and greet them
+        // String name;
+        // System.out.println("Enter your name : ");
+        // name = sc.nextLine();
+        // System.out.println("Hello "+name+", Have a good day...:)");
+
+
+        //question 4 : convert kilometers to mile 
+        // double distance;
+        // System.out.print("Enter the distance in kms : ");
+        // distance = sc.nextDouble();
+        // System.out.println("\n"+ distance + " km = "+ (distance*0.621371) + " miles");
+
+
+        //question 5 : detect whether the no. entered is integer or not
+        // System.out.println("Enter any no. ");
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println(sc.hasNextInt());
+
+        // sc.close();
+    }
+}
