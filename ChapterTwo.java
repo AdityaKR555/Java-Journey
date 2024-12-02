288dbe039e1d5404ecb01a0f61a076ee65b3be8c
Aditya Kumar <adityaforwork101@gmail.com>
2024-12-02 16:02:12 +0530

    Arithmetic problems

---
+++ ChapterTwo.java
@@ -0,0 +1,35 @@
+// import java.util.Scanner;
+class ChapterTwo{
+    public static void main(String args[]) {
+        // question 1: solving an arithmetic problem
+        // float a = (7/4.0f)*(9/2.0f);
+        // System.out.println("Result = "+a);
+
+        
+        // question 2 : encrypt the grade by 8 and again decrypt it
+        // Scanner sc = new Scanner(System.in);
+        // char grade;
+        // System.out.println("Enter the grade : ");
+        // grade = sc.next().charAt(0);
+        // //encryption by 8
+        // grade = (char)(grade+8);
+        // System.out.println(grade);
+        // //decryption by 8
+        // grade = (char)(grade-8);
+        // System.out.println(grade);
+
+
+        //question 3 : check if user input is greater than 555 or not
+        // System.out.println("Enter the no. baby : ");
+        // int a = sc.nextInt();
+        // System.out.println(a>555);
+
+
+        //question 4 : writing an expression
+        // float v=9, u=3, a=3, s=5;
+        // float ans = ((v*v)-(u*u))/(2*a*s);
+        // System.out.println(ans);
+
+        // sc.close();
+    }
+}
