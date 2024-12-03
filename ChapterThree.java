0743488235a0a29de16cd5eea867f74e975206b4
Aditya Kumar <adityaforwork101@gmail.com>
2024-12-03 23:02:35 +0530

    About Strings

---
+++ ChapterThree.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ChapterThree {
+    public static void main(String[] args) {
+        //question 1 : convert a string to lowercase and uppercase
+       // String action;
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a string : ");
+        // action = sc.nextLine();
+        // System.out.println(action.toLowerCase());
+        // System.out.println(action.toUpperCase());
+
+
+        //question 2 : replace spaces with underscores
+        // String letsGo;
+        // System.out.println("Enter string : ");
+        // letsGo = sc.nextLine();
+        // System.out.println(letsGo.replace(" ", "_"));
+
+
+        //question 3 : replacement of name from a letter
+        // String letter = "Dear <|name|>, Thanks a lot <|name|>";
+        // letter = letter.replace("<|name|>","Chotu Babu");
+        // System.out.println(letter);
+
+
+        //question 4 : to detect double and triple spaces in a string
+        // System.out.println("Enter a string : ");
+        // String krsna = sc.nextLine();
+        // System.out.println(krsna.indexOf("  "));
+        // System.out.println(krsna.indexOf("   "));
+
+
+        //question 5 : format using escape sequence characters(\n,\t)
+        // String letter = "Dear Aditya,\tYou are really very good at it,\nMan...u r really awesome!!!";
+        // System.out.println(letter);
+
+        sc.close();
+
+    }
+}
