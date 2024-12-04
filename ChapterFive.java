531eccb91700e2ab52017140366ede4a437a5934
Aditya Kumar <adityaforwork101@gmail.com>
2024-12-04 22:32:49 +0530

    Loops problems

---
+++ ChapterFive.java
@@ -0,0 +1,83 @@
+public class ChapterFive {
+    public static void main(String[] args) {
+        //Question 1 : sum of first n even no.s
+
+        // int n, sum=0;
+        // System.out.print("Enter limit : ");
+        // Scanner sc = new Scanner(System.in);
+        // n = sc.nextInt();
+        // int i = 1;
+        // while(i<=n){
+        //     if(i%2==0) sum+=i;
+        //     i++;
+        // }
+        // System.out.println("Sum = "+sum);
+
+
+        //Questio 2 : multiplication table of given no. n
+
+        // int n;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a no. to print table of : ");
+        // n = sc.nextInt();
+        // for(int i = 1; i<=10; i++){
+        //     System.out.println(n*i);
+        // }
+
+        //Question 3 : multipication table in reverse order 
+        
+        // int n;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a no. to print table of in reverse order : ");
+        // n = sc.nextInt();
+        // for(int i = 10; i>=1; i--){
+        //     System.out.println(n*i);
+        // }
+
+
+        //Question 4 : find factorial using for loop
+      
+        // int n, product=1;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a no. to find factorial of : ");
+        // n = sc.nextInt();
+        // for(int i = 1; i<=n; i++){
+        //     product*=i;
+        // }
+        // System.out.println(product);
+
+
+        //Question 5 : factorial using while loop
+        // int n, product=1;
+        // int i=1;
+        // System.out.print("Eneter a no. to find factorial of : ");
+        // Scanner sc = new Scanner(System.in);
+        // n = sc.nextInt();
+        // while(i<=n){
+        //     product*=i;
+        //     i++;
+        // }
+        // System.out.println(product);
+
+
+        //Question 6 : printing a pattern using for loop
+        
+        // for(int i=1; i<=4; i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println("");
+        // }
+
+
+        //Question 7 : sum of no.s that occur in multiplication table of 8
+        // int sum=0;
+        // // int mt;
+        // for(int i=1; i<=10; i++){
+        //     // mt = 8*i;
+        //     sum=sum+(8*i);
+        // }
+        // System.out.println("Sum = "+sum);
+
+    }
+}
