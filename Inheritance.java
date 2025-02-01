871aedd7da92defa76439ae77ee9e96ee31da79d
Aditya Kumar <adityaforwork101@gmail.com>
2025-02-01 11:31:45 +0530

    merge fixed

--- Inheritance.java
+++ Inheritance.java
@@ -1,67 +1,67 @@
 
-class try1{
-    int x;
-    public void setX(int x){
-        this.x = x;
-    }
-    public int getX(){
-        return this.x;
-    }
-}
-//Inheritance
-class try2 extends try1{
-    int y;
-    public void setY(int y){
-        this.y = y;
-    }
-    public int getY(){
-        return this.y;
-    }
-}
+// class try1{
+//     int x;
+//     public void setX(int x){
+//         this.x = x;
+//     }
+//     public int getX(){
+//         return this.x;
+//     }
+// }
+// //Inheritance
+// class try2 extends try1{
+//     int y;
+//     public void setY(int y){
+//         this.y = y;
+//     }
+//     public int getY(){
+//         return this.y;
+//     }
+// }
 
-class Base{
-    Base(){
-        System.out.println("I m a constructor!");
-    }
-    Base(int x){
-        System.out.println("I am 2nd constructor!");
-    }
-}
-//Constructor in inheritance
-class Derived extends Base{
-    Derived(){
-        System.out.println("I m a derived class constructor!");
-    }
-    Derived(int x, int y){
-        super(x);
-        System.out.println("I am 2nd derived Constructor!");
-    }
-}
+// class Base{
+//     Base(){
+//         System.out.println("I m a constructor!");
+//     }
+//     Base(int x){
+//         System.out.println("I am 2nd constructor!");
+//     }
+// }
+// //Constructor in inheritance
+// class Derived extends Base{
+//     Derived(){
+//         System.out.println("I m a derived class constructor!");
+//     }
+//     Derived(int x, int y){
+//         super(x);
+//         System.out.println("I am 2nd derived Constructor!");
+//     }
+// }
 
-public class Inheritance {
-    public static void main(String[] args) {
+// public class Inheritance {
+//     public static void main(String[] args) {
         
-        try1 obj1 = new try1();
+//         try1 obj1 = new try1();
 
-        obj1.setX(7);                              //Allowed
-        System.out.println(obj1.getX());
+//         obj1.setX(7);                              //Allowed
+//         System.out.println(obj1.getX());
 
-        // obj1.setY(3);
-        // System.out.println(obj1.getx());          //Not Allowed
+//         // obj1.setY(3);
+//         // System.out.println(obj1.getx());          //Not Allowed
 
-        try2 obj2 = new try2();
+//         try2 obj2 = new try2();
 
-        obj2.setX(8);                              //Allowed
-        System.out.println(obj2.getX());             
+//         obj2.setX(8);                              //Allowed
+//         System.out.println(obj2.getX());             
 
-        obj2.setY(2);                              //Allowed
-        System.out.println(obj2.getY());
+//         obj2.setY(2);                              //Allowed
+//         System.out.println(obj2.getY());
 
-        Derived d = new Derived(3,4);
+//         Derived d = new Derived(3,4);
         
 
 
 
-    }
-}
+//     }
+// }
 
